da70517e20b9fa23b54e49bc323a38220bb15f01
hizkielT <hezekiel.feyissa@gmail.com>
2022-10-18 19:42:01 +0300

    added color nad screen types

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRed: 'hsl(12, 88%, 59%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+      },
+    },
   },
   plugins: [],
 }
